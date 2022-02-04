0b68db885b90705c6f796ea0c01c4ed85d67fe10
Rayan Bouamrane <rayan.bouamrane@cgi.com>
2022-02-04 14:59:25 -0500

    added RafaToRoger test to HelloSeleniumTest.java

--- src/test/java/com/xyzcorp/HelloSeleniumTest.java
+++ src/test/java/com/xyzcorp/HelloSeleniumTest.java
@@ -3,24 +3,29 @@ package com.xyzcorp;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSeleniumTest {
 
-    private ChromeDriver chromeDriver;
+    private static ChromeDriver chromeDriver;
 
     @BeforeAll
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\rayan.bouamrane\\Desktop\\chromedriver_win32");
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\rayan.bouamrane\\Desktop\\chromedriver_win32\\chromedriver.exe");
         chromeDriver = new ChromeDriver();
     }
 
     @Test
-    public void testPage() {
-
+    public void testWikipediaLinkClick() {
+        chromeDriver.get("https://en.wikipedia.org/wiki/Rafael_Nadal");
+        chromeDriver.manage().window().setSize(new Dimension(807, 824));
+        chromeDriver.findElement(By.linkText("Roger Federer")).click();
     }
+
     @AfterAll
-    public void cleanup(){
+    public static void cleanup() {
         chromeDriver.close();
     }
 
